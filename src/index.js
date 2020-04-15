fb904d491fc602ce84bd8036ff25da152d4d619a
Andrew <andrewtong0@gmail.com>
2020-04-15 14:11:38 -0700

    Added autoplay video

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Player } from 'video-react';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Player
+      playsInline
+      autoPlay={true}
+      poster="/assets/poster.png"
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
